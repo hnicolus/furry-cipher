bec7e57de942623d299cad8d97d49b15934d1a7d
Nicolas <nicolusmaluleke@gmail.com>
2022-11-11 00:34:31 +0200

    Renamed file.

---
+++ src/console/FurryCipher/CipherCommands.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace FurryCipher;
+public class CipherCommands
+{
+    public string DoCipher(string option, string text, int key)
+    {
+        var executionOptions = new Dictionary<string, Func<string, int, string>>();
+        executionOptions.Add(MenuOption.EncryptMessage, (message, key) => EncryptMessage(message, key));
+        executionOptions.Add(MenuOption.DecryptMessage, (message, key) => DecryptMessage(message, key));
+
+        if (!executionOptions.ContainsKey(option)) return "";
+
+        return executionOptions[option](text, key);
+    }
+
+    private string DecryptMessage(string message, int key) =>
+        CCipher.FuriousCipher.Decrypt(message, key);
+
+    private string EncryptMessage(string message, int key) =>
+        CCipher.FuriousCipher.Encrypt(message, key);
+}
